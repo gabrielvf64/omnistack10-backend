afed69a2752cf8b934e4f0964c5a5df108be9161
gabrielvf64 <gabrielvf64@gmail.com>
2020-01-14 11:54:05 -0300

    Test query param

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
+app.get('/users', (request, response) => {
+    console.log(request.query)
     return response.json({ message: 'olá'})
 })
 
