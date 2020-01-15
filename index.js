c9e3ed1bffcf2934aa22545e6f1da1317c7f3db9
gabrielvf64 <gabrielvf64@gmail.com>
2020-01-14 21:10:03 -0300

    Param types and route examples

--- index.js
+++ index.js
@@ -2,9 +2,30 @@ const express = require('express');
 
 const app = express();
 
+// Express, use o formato json
+app.use(express.json());
+
+// Métodos HTTP: GET, POST, PUT, DELETE
+
+// Tipos de parâmetros:
+
+// Query Params: request.query (Filtros, ordenação, paginação)
+// Route Params: request.params (Identificar um recurso na alteração ou remoção)
+// Body: request.body (Dados para criação ou alteração de um registro)
+
 app.get('/users', (request, response) => {
     console.log(request.query)
     return response.json({ message: 'olá'})
-})
+});
+
+app.delete('/users/:id', (request, response) => {
+    console.log(request.params)
+    return response.json({ message: 'olá'})
+});
+
+app.post('/users', (request, response) => {
+    console.log(request.body)
+    return response.json({ message: 'olá'})
+});
 
 app.listen(3333);
