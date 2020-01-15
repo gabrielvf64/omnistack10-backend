b024af490c7ef29a9fbd6d01ec892409c31097b5
gabrielvf64 <gabrielvf64@gmail.com>
2020-01-14 21:37:19 -0300

    Conection to the non relational database

--- index.js
+++ index.js
@@ -1,27 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
+// MongoDB (Não-relacional)
+// Mongoose para conversar o node com mongo
+
+// Conectado com o banco remoto
+mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-b6kom.mongodb.net/week10?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
 // Express, use o formato json
 app.use(express.json());
-
-// Métodos HTTP: GET, POST, PUT, DELETE
-
-// Tipos de parâmetros:
-
-// Query Params: request.query (Filtros, ordenação, paginação)
-// Route Params: request.params (Identificar um recurso na alteração ou remoção)
-// Body: request.body (Dados para criação ou alteração de um registro)
-
-app.get('/users', (request, response) => {
-    console.log(request.query)
-    return response.json({ message: 'olá'})
-});
-
-app.delete('/users/:id', (request, response) => {
-    console.log(request.params)
-    return response.json({ message: 'olá'})
-});
 
 app.post('/users', (request, response) => {
     console.log(request.body)
