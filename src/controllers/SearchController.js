7d2aeef2cac19cb3d619da06d8f174bb54b2ea4f
gabrielvf64 <gabrielvf64@gmail.com>
2020-01-16 14:05:08 -0300

    Filter by techs working

--- src/controllers/SearchController.js
+++ src/controllers/SearchController.js
@@ -2,13 +2,17 @@ const Dev = require('../models/Dev');
 const parseStringAsArray = require('../utils/parseStringAsArray');
 
 module.exports = {
-    async index(request, response) {
-        const { latitude, longitude, techs } = request.query;
+  async index(request, response) {
+    const { latitude, longitude, techs } = request.query;
 
-        const techsArray = parseStringAsArray(techs);
+    const techsArray = parseStringAsArray(techs);
 
-        console.log(techsArray);
+    const devs = await Dev.find({
+      techs: {
+        $in: techsArray,
+      },
+    });
 
-        return response.json({ devs: [] });
-    }
-}
+    return response.json({ devs });
+  }
+};
