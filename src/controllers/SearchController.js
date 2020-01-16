6b34b1f9828ba36005abfc1fb77c9f7717c20b86
gabrielvf64 <gabrielvf64@gmail.com>
2020-01-16 14:33:50 -0300

    Add location filter; fixed location index value

--- src/controllers/SearchController.js
+++ src/controllers/SearchController.js
@@ -11,6 +11,15 @@ module.exports = {
       techs: {
         $in: techsArray,
       },
+      location: {
+        $near: {
+          $geometry: {
+            type: 'Point',
+            coordinates: [longitude, latitude],
+          },
+          $maxDistance: 10000,
+        }
+      }
     });
 
     return response.json({ devs });
