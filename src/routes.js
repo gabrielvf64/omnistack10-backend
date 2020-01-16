0de57f7d1ed5b02798c8f6b92a82d5e576d08c66
gabrielvf64 <gabrielvf64@gmail.com>
2020-01-16 10:26:23 -0300

    Created dev object

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,27 @@
 const { Router } = require('express');
 const axios  = require('axios');
+const Dev = require('./models/Dev');
 
 const routes = Router();
 
 routes.post('/devs', async (request, response) => {
-    const { github_username } = request.body;
+    const { github_username, techs } = request.body;
 
     const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
 
-    console.log(apiResponse.data);
+    const { name = loing, avatar_url, bio } = apiResponse.data;
 
-    return response.json({ message: 'olá'})
+    const techsArray = techs.split(',').map(tech => tech.trim());
+
+    const dev = await Dev.create({
+        github_username,
+        name,
+        avatar_url,
+        bio,
+        techs: techsArray,
+    });
+
+    return response.json(dev);
 });
 
 module.exports = routes;
