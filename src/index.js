156cb427634e4d1f8aba9aaf513b4bccfaadc256
gabrielvf64 <gabrielvf64@gmail.com>
2020-01-18 21:06:13 -0300

    Add cors to set the client address

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const routes = require('./routes');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const routes = require("./routes");
 
 const app = express();
 
-mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-b6kom.mongodb.net/week10?retryWrites=true&w=majority', {
+mongoose.connect(
+  "mongodb+srv://omnistack:omnistack@cluster0-b6kom.mongodb.net/week10?retryWrites=true&w=majority",
+  {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
+    useUnifiedTopology: true
+  }
+);
 
+app.use(cors({ origin: "http://localhost:3000" }));
 app.use(express.json());
 app.use(routes);
 
