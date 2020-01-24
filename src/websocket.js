9d36f1f8859d5e58a397092c68c563d505b540b7
gabrielvf64 <gabrielvf64@gmail.com>
2020-01-24 11:30:11 -0300

    Created arrary to store the connections

--- src/websocket.js
+++ src/websocket.js
@@ -1,16 +1,21 @@
 const socketio = require('socket.io');
+const parseStringAsArray = require('./utils/parseStringAsArray');
+
+const connections = [];
 
 exports.setupWebsocket = server => {
   const io = socketio(server);
 
   io.on('connection', socket => {
-    console.log(socket.id);
-    // parâmetros enviados pelo front
-    console.log(socket.handshake.query);
+    const { latitude, longitude, techs } = socket.handshake.query;
 
-    // backend enviando info pro front sem o front ter feito uma req
-    setTimeout(() => {
-      socket.emit('message', 'ola');
-    }, 3000);
+    connections.push({
+      id: socket.id,
+      coordinates: {
+        latitude: Number(latitude),
+        longitude: Number(longitude)
+      },
+      techs: parseStringAsArray(techs)
+    });
   });
 };
