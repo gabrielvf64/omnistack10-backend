0da226094d8102ca4236b8e27d4c1d25aa18c797
gabrielvf64 <gabrielvf64@gmail.com>
2020-01-24 10:43:03 -0300

    Websocket setup; getting the connection id

--- src/websocket.js
+++ src/websocket.js
@@ -1,3 +1,9 @@
 const socketio = require('socket.io');
 
-exports.setupWebsocket = server => {};
+exports.setupWebsocket = server => {
+  const io = socketio(server);
+
+  io.on('connection', socket => {
+    console.log(socket.id);
+  });
+};
